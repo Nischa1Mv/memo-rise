16847c29f96d099825e5be939b94f7ed55b25308
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-01-20 17:41:09 +0530

    changed  bg color

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-black dark:from-black dark:to-gray-900">
+    <div className="min-h-screen bg-[#09090b] dark:from-black dark:to-gray-900">
       {children}
     </div>
   );
