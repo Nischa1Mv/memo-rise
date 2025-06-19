944059c7904c47e803fa1907ec8672062aead5cf
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-06-19 10:15:15 +0000

    changed the title of the app, like in the tab area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Note Taking App",
+  title: "MemoRise",
   description: "A simple note-taking application",
 };
 
