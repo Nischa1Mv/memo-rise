21213a978a91d3053ebb40e21ba861fbfebfdeec
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2025-06-19 11:50:10 +0000

    added new favicons and added to the meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,22 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+import type { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "MemoRise",
   description: "A simple note-taking application",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png" },
+      { url: "/favicon-32x32.png" },
+      { url: "/favicon.ico" }
+    ],
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico"
+  }
 };
+
 
 export default function RootLayout({
   children,
